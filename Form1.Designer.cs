effeb4238ff5d51153ccfac18e90f6654c48bec1
John Doe <106913591+ZiadAhmed-11@users.noreply.github.com>
2023-12-25 17:07:06 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace ComputerSecurity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comment = new System.Windows.Forms.TextBox();
+            this.ceaser = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.key1 = new System.Windows.Forms.TextBox();
+            this.mono = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pfkey = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comment
+            // 
+            this.comment.Location = new System.Drawing.Point(187, 103);
+            this.comment.Name = "comment";
+            this.comment.Size = new System.Drawing.Size(520, 20);
+            this.comment.TabIndex = 0;
+            this.comment.TextChanged += new System.EventHandler(this.comment_TextChanged);
+            // 
+            // ceaser
+            // 
+            this.ceaser.Location = new System.Drawing.Point(153, 201);
+            this.ceaser.Name = "ceaser";
+            this.ceaser.Size = new System.Drawing.Size(520, 20);
+            this.ceaser.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(153, 292);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(520, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(153, 375);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(520, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Montserrat Medium", 14F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(57, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 26);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Comment:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Montserrat Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(62, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Caesar :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Montserrat Medium", 12F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(62, 288);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Playfair :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("Montserrat Medium", 12F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(62, 367);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Mono :";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Location = new System.Drawing.Point(387, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Montserrat Medium", 8F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(163, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Key :";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // key1
+            // 
+            this.key1.Location = new System.Drawing.Point(200, 175);
+            this.key1.Name = "key1";
+            this.key1.Size = new System.Drawing.Size(100, 20);
+            this.key1.TabIndex = 9;
+            // 
+            // mono
+            // 
+            this.mono.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.mono.Location = new System.Drawing.Point(387, 346);
+            this.mono.Name = "mono";
+            this.mono.Size = new System.Drawing.Size(75, 23);
+            this.mono.TabIndex = 11;
+            this.mono.Text = "convert";
+            this.mono.UseVisualStyleBackColor = true;
+            this.mono.Click += new System.EventHandler(this.mono_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Fontastique", 35F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.label6.Location = new System.Drawing.Point(288, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(247, 58);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Encryption";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(306, 178);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(64, 17);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "Decrypt";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(306, 350);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(64, 17);
+            this.checkBox2.TabIndex = 14;
+            this.checkBox2.Text = "Decrypt";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Montserrat Medium", 8F, System.Drawing.FontStyle.Bold);
+            this.label7.Location = new System.Drawing.Point(163, 271);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 15);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Key :";
+            // 
+            // pfkey
+            // 
+            this.pfkey.Location = new System.Drawing.Point(200, 269);
+            this.pfkey.Name = "pfkey";
+            this.pfkey.Size = new System.Drawing.Size(100, 20);
+            this.pfkey.TabIndex = 16;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button2.Location = new System.Drawing.Point(387, 263);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "convert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(306, 271);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(64, 17);
+            this.checkBox3.TabIndex = 18;
+            this.checkBox3.Text = "Decrypt";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ComputerSecurity.Properties.Resources.istockphoto_1442598753_612x612_removebg_preview;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(96, 62);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackgroundImage = global::ComputerSecurity.Properties.Resources.e67407f402881f2aac3641d889292884;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.comment);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pfkey);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.mono);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.key1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.ceaser);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Encryption|Decryption";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox comment;
+        private System.Windows.Forms.TextBox ceaser;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox key1;
+        private System.Windows.Forms.Button mono;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox pfkey;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,379 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComputerSecurity
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comment_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (key1.Text.Length == 0)
+            {
+                ceaser.Text = "Please Enter The Key!!";
+                ceaser.ForeColor = Color.Red;
+            }
+
+            else
+            {
+                int shift = int.Parse(key1.Text);
+                string inputComment = comment.Text;
+                ceaser.ForeColor = Color.Green;
+                if (checkBox1.Checked)
+                {
+                    string DecryptedText = Dencrypt(inputComment, shift);
+                    ceaser.Text = DecryptedText;
+                }
+                else
+                {
+
+                    string encryptedText = Encrypt(inputComment, shift);
+                    ceaser.Text = encryptedText;
+                }
+            }
+        }
+
+        private static string Encrypt(string input, int shift)
+        {
+            char[] result = new char[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.IsLetter(input[i]))
+                {
+                    char offset = char.IsUpper(input[i]) ? 'A' : 'a';
+                    result[i] = (char)((input[i] + shift - offset) % 26 + offset);
+                }
+                else
+                {
+                    // If the character is not a letter, keep it unchanged
+                    result[i] = input[i];
+                }
+            }
+
+            return new string(result);
+        }
+
+        private static string Dencrypt(string input, int shift)
+        {
+            char[] result = new char[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.IsLetter(input[i]))
+                {
+                    char offset = char.IsUpper(input[i]) ? 'A' : 'a';
+                    result[i] = (char)((input[i] - shift - offset + 26) % 26 + offset);
+                }
+                else
+                {
+                    // If the character is not a letter, keep it unchanged
+                    result[i] = input[i];
+                }
+            }
+
+            return new string(result);
+        }
+
+        private void mono_Click(object sender, EventArgs e)
+        {
+            Dictionary<char, char> encryptionMap = CreateEncryptionMap();
+            Dictionary<char, char> decryptionMap = CreateDencryptionMap();
+
+            string Mono = comment.Text.ToUpper();
+            if (checkBox2.Checked)
+            {
+                string DecryptionText = EncryptOrDecrypt(Mono, decryptionMap);
+                textBox4.Text = DecryptionText;
+
+            }
+            else
+            {
+                string encryptedText = EncryptOrDecrypt(Mono, encryptionMap);
+                textBox4.Text = encryptedText;
+
+            }
+            textBox4.ForeColor = Color.Green;
+        }
+        static Dictionary<char, char> CreateEncryptionMap()
+        {
+            // Create a simple encryption mapping (you can customize this)
+            string plainAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string cipherAlphabet = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+
+            Dictionary<char, char> encryptionMap = new Dictionary<char, char>();
+
+            for (int i = 0; i < plainAlphabet.Length; i++)
+            {
+                encryptionMap.Add(plainAlphabet[i], cipherAlphabet[i]);
+            }
+
+            return encryptionMap;
+        }
+
+        static string EncryptOrDecrypt(string input, Dictionary<char, char> encryptionMap)
+        {
+            // Encrypt the input text using the provided mapping
+            char[] result = new char[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentChar = input[i];
+
+                // Check if the character is in the mapping
+                if (encryptionMap.ContainsKey(currentChar))
+                {
+                    // Replace with the corresponding mapped character
+                    result[i] = encryptionMap[currentChar];
+                }
+                else
+                {
+                    // If not in the mapping, keep the character unchanged
+                    result[i] = currentChar;
+                }
+            }
+
+            return new string(result);
+        }
+
+
+        static Dictionary<char, char> CreateDencryptionMap()
+        {
+            // Create a simple encryption mapping (you can customize this)
+            string plainAlphabet = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+            string cipherAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            Dictionary<char, char> encryptionMap = new Dictionary<char, char>();
+
+            for (int i = 0; i < plainAlphabet.Length; i++)
+            {
+                encryptionMap.Add(plainAlphabet[i], cipherAlphabet[i]);
+            }
+
+            return encryptionMap;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string key = pfkey.Text.ToUpper();
+            if (key.Length == 0)
+            {
+                textBox3.Text = "Please, Enter string key!!";
+                textBox3.ForeColor = Color.Red;
+            }
+            else
+            {
+                char[,] matrix = CreateMatrix(key);
+                string inputText = comment.Text.ToUpper().Replace("J", "I");
+
+                // Insert "X" between consecutive identical characters
+                inputText = InsertXBetweenIdenticalCharacters(inputText);
+                string encryptedText = Encrypt(inputText, matrix);
+
+/*                string encryptedText = ProcessText(inputText, matrix, true);
+*/                /*                string encryptedText = Encrypt(inputText, matrix);
+                */
+                if (checkBox3.Checked)
+                {
+                    string decryptedText = Encrypt(inputText, matrix);
+                    textBox3.Text = decryptedText;
+                }
+                else
+                    textBox3.Text = encryptedText;
+                textBox3.ForeColor = Color.Green;
+
+
+            }
+        }
+        static string InsertXBetweenIdenticalCharacters(string input)
+        {
+            // Insert "X" between consecutive identical characters
+            string modifiedText = "";
+            char previousChar = '\0';//hello
+
+            foreach (char currentChar in input)
+            {
+
+                if (currentChar == previousChar)
+                {
+                    modifiedText += 'X';
+                }
+                modifiedText += currentChar;
+
+                previousChar = currentChar;
+            }
+
+            return modifiedText;
+        }
+        static char[,] CreateMatrix(string key)
+        {
+            // Create a matrix to store the Playfair key
+            char[,] matrix = new char[5, 5];
+            string keyWithoutDuplicates = RemoveDuplicateChars(key + "ABCDEFGHIKLMNOPQRSTUVWXYZ");
+
+            int k = 0;
+
+            // Fill the matrix with the key
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    matrix[i, j] = keyWithoutDuplicates[k];
+                    k++;
+                }
+            }
+
+            return matrix;
+        }
+
+        static string RemoveDuplicateChars(string input)
+        {
+            // Remove duplicate characters from the input string
+            HashSet<char> uniqueChars = new HashSet<char>(input);
+            return new string(uniqueChars.ToArray());
+        }
+
+        static string Encrypt(string input, char[,] matrix)
+        {
+            string encryptedText = "";
+
+            for (int i = 0; i < input.Length; i += 2)
+            {
+                char firstChar = input[i];
+                char secondChar = (i + 1 < input.Length) ? input[i + 1] : 'X'; // Add 'X' if the input length is odd
+
+                int[] firstPosition = FindPosition(matrix, firstChar);
+                int[] secondPosition = FindPosition(matrix, secondChar);
+
+                // If the letters are in the same row
+                if (firstPosition[0] == secondPosition[0])
+                {
+                    firstPosition[1] = (firstPosition[1] + 1) % 5;
+                    secondPosition[1] = (secondPosition[1] + 1) % 5;
+                }
+                // If the letters are in the same column
+                else if (firstPosition[1] == secondPosition[1])
+                {
+                    firstPosition[0] = (firstPosition[0] + 1) % 5;
+                    secondPosition[0] = (secondPosition[0] + 1) % 5;
+                }
+                // If the letters form a rectangle
+                else
+                {
+                    int temp = firstPosition[1];
+                    firstPosition[1] = secondPosition[1];
+                    secondPosition[1] = temp;
+                }
+
+                encryptedText += matrix[firstPosition[0], firstPosition[1]];
+                encryptedText += matrix[secondPosition[0], secondPosition[1]];
+            }
+
+            return encryptedText;
+        }
+
+        static int[] FindPosition(char[,] matrix, char target)
+        {
+            // Find the position of the target character in the matrix
+            int[] position = new int[2];
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (matrix[i, j] == target)
+                    {
+                        position[0] = i;
+                        position[1] = j;
+                        return position;
+                    }
+                }
+            }
+
+            return position;
+        }
+
+        //Decrypt and Encrypt
+        /*static string ProcessText(string input, char[,] matrix, bool encrypt)
+        {
+            string processedText = "";
+            int direction = encrypt ? 1 : -1;
+
+            for (int i = 0; i < input.Length; i += 2)
+            {
+                char firstChar = input[i];
+                char secondChar = (i + 1 < input.Length) ? input[i + 1] : 'X'; // Add 'X' if the input length is odd
+
+                int[] firstPosition = FindPosition(matrix, firstChar);
+                int[] secondPosition = FindPosition(matrix, secondChar);
+
+                // Adjust positions based on encryption/decryption direction
+                firstPosition[0] = (firstPosition[0] + direction + 5) % 5;
+                firstPosition[1] = (firstPosition[1] + direction + 5) % 5;
+                secondPosition[0] = (secondPosition[0] + direction + 5) % 5;
+                secondPosition[1] = (secondPosition[1] + direction + 5) % 5;
+
+                processedText += matrix[firstPosition[0], firstPosition[1]];
+                processedText += matrix[secondPosition[0], secondPosition[1]];
+            }
+
+            return processedText;
+        }*/
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
